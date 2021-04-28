02f9e7c856db7c147a61748758e7537f97f10ca8
Пользователь <ernis4444e@gmail.com>
2021-04-28 14:15:15 +0600

    dobavil tish

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <div>
+            <h1>TISH</h1>
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+
+export default header;
